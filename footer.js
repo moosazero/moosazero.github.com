c6521509fc467e6e511425c4e6a4afa61e80c7be
moosa esfahanian <moosa_es@unknown00236C9A62AC.gateway.2wire.net>
2012-08-30 12:16:18 -0500

    header footer

--- footer.js
+++ footer.js
@@ -1,1 +1,5 @@
-Next element matching (regexp): 
+(function(){
+var footer='</div><div id="footer"><p><a href="index.html">Home</a> | <a href="about.html">About Me</a> | <a href="contact.html">Contact Me</a> </p>';
+	  	
+document.write(footer);
+}()) 
